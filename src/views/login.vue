819a47791550c6466afe9ccd97eb2de7abcb3a1d
unknown <1294470191@qq.com>
2019-07-22 19:46:37 +0800

    cli基础结构

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,108 +1,13 @@
 <template>
-  <div class="login">
-    <div class="container">
-      <img src="../assets/avatar.jpg" alt="" class="avatar">
-      <el-form :model="loginForm" :rules="rules" ref="loginForm" class="demo-ruleForm">
-        <el-form-item prop="username">
-          <el-input v-model="loginForm.username" placeholder="用户名" prefix-icon="myicon myicon-user"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input v-model="loginForm.password" placeholder="密码"  prefix-icon="myicon myicon-key"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" class="login-btn" @click='login'>登陆</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-  </div>
+<div id="app">这是一个根目录</div>
 </template>
+
 <script>
-import { login } from '@/api/user_index.js'
 export default {
-  data () {
-    return {
-      loginForm: {
-        username: 'admin',
-        password: '123456'
-      },
-      rules: {
-        username: [
-          { required: true, message: '请输入用户名', trigger: 'blur' }
-        ],
-        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
-      }
-    }
-  },
-  methods: {
-    login () {
-      // 再次实现数据的验证
-      // 我们可以调用表单的validate方法实现数据的验证,在验证完成的时候,会调用传入的回调函数,这个回调函数有一个参数valid,如果这个参数为true,则说明验证通过(可以继续你的登陆请求),否则验证失败(阻止你的登陆请求并给出提示)
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          login(this.loginForm)
-            .then(res => {
-              console.log(res)
-              if (res.data.meta.status === 200) {
-                // 进行路由跳转
-                this.$router.push({ name: 'home' })
-              } else {
-                this.$message({
-                  message: res.data.meta.msg,
-                  type: 'error'
-                })
-              }
-            })
-            .catch(err => {
-              console.log(err)
-              this.$message({
-                message: '登陆失败',
-                type: 'error'
-              })
-            })
-        } else {
-          this.$message({
-            message: '数据输入不合法',
-            type: 'error'
-          })
-          // 只有return false才能阻止请求
-          return false
-        }
-      })
-      // 发起请求
-    }
-  }
+
 }
 </script>
-<style lang="less" scoped>
-.login {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  background-color: #2f4050;
-  .container {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 400px;
-    padding: 0px 40px 15px 40px;
-    margin: 200px auto;
-    background: white;
-    .avatar {
-      position: relative;
-      left: 50%;
-      width: 120px;
-      height: 120px;
-      margin-left: -60px;
-      margin-top: -60px;
-      box-sizing: border-box;
-      border-radius: 50%;
-      border: 10px solid #fff;
-      box-shadow: 0 1px 5px #ccc;
-      overflow: hidden;
-    }
-    .login-btn {
-      width: 100%;
-    }
-  }
-}
+
+<style>
+
 </style>
